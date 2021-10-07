b687a43c9d051484e964d517f09cc5969269229d
Mateusz <mateuszzygadlo00@gmail.com>
2021-10-07 23:29:18 +0200

    Adding HomeContent component and responsive styling

--- shopping-card/src/components/App.js
+++ shopping-card/src/components/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import WelcomeContent from './WelcomeContent';
 
 const App = () => {
   return(
     <div>
       <Navbar />
+      <WelcomeContent />
     </div>
   )
 }
---
+++ shopping-card/src/components/WelcomeContent.js
@@ -0,0 +1,10 @@
+const WelcomeContent = () => {
+    return(
+        <div className="welcome-content">
+            <h1>Welcome to my shop</h1>
+            <button>go to shop</button>
+        </div>
+    )
+}
+
+export default WelcomeContent;
