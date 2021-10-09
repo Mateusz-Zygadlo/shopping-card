aa84150ba7de9053682fc88952699eb81e41cb08
Mateusz <mateuszzygadlo00@gmail.com>
2021-10-09 13:46:12 +0200

    Adding the ability to pass props to components

--- shopping-card/src/components/Routes.js
+++ shopping-card/src/components/Routes.js
@@ -2,14 +2,36 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from '../components/pages/HomePage';
 import App from './App';
 import ProductsPage from './pages/ProductsPage';
+import ShoppingCardPage from './pages/ShoppingCardPage';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Switch>
-                <Route exact path='/' component={App} />
-                <Route exact path='/home' component={HomePage} />
-                <Route exact path='/products' component={ProductsPage} />
+                <Route 
+                    exact 
+                    path='/' 
+                    render={(props) => (
+                        <App />
+                    )} />
+                <Route 
+                    exact 
+                    path='/home' 
+                    render={(props) => (
+                        <HomePage />
+                    )} />
+                <Route 
+                    exact 
+                    path='/products' 
+                    render={(props) => (
+                        <ProductsPage />
+                    )} />
+                <Route 
+                    exact 
+                    path='/shopping-card' 
+                    render={(props) => (
+                        <ShoppingCardPage />
+                    )} />
             </Switch>
         </BrowserRouter>
     )
---
+++ shopping-card/src/components/pages/ShoppingCardPage.js
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar";
+
+const ShoppingCardPage = (props) => {
+    return(
+        <div>
+            <Navbar />
+            <div>
+                Hello shopping card page
+            </div>
+        </div>
+    )
+}
+
+export default ShoppingCardPage;
